294c727508ed5f2df153f9cfddd64263d795b298
lambdv <scafiy17@gmail.com>
2025-08-02 21:11:39 +1200

    added decorator method and more component classes

--- framework/node.ts
+++ framework/node.ts
@@ -1,5 +1,4 @@
-export type Prop = any
-// {
+export type Prop = any; // {
 //     title?: string,
 //     innterText?: string,
 //     outerText?: string,
@@ -16,7 +15,7 @@ export abstract class Component{
     children: Component[];
     props: any;
     abstract type: string;
-    
+
     constructor(children?: Component[], props?: Prop) {
         this.children = children || [];
         this.props = props || {};
@@ -24,22 +23,33 @@ export abstract class Component{
     
     render(): HTMLElement {
         let x = document.createElement(this.type);
-        x.innerText = this.props.innterText || "";
+        x.innerText = this.props.innerText || "";
         x.className = this.props.className || "";
-        x.setAttribute("href", this.props.href || "")
-        x.setAttribute("target", this.props.target || "")
+        // x.setAttribute("href", this.props.href || "")
+        // x.setAttribute("target", this.props.target || "")
 
+        for (const [key, value] of Object.entries(this.props)) {
+            x.setAttribute(key, String(value));
+        }
         for (const child of this.children) {
             if (child) x.appendChild(child.render());
         }
+
         if (this.props.onClick) {
             x.addEventListener("click", (e) => {
                 this.props.onClick?.call(e);    
             });
         }
+        x = this.decorate(x);
         return x;
     }
 
+    decorate(x: HTMLElement){
+        return x;
+    }
+
+
+    
     static of(type: string, children?: Component[], props?: Prop): Component{
         return new class extends Component {
             type: string = type;
@@ -66,6 +76,14 @@ export class A extends Component {
     type: string = "a";
 }
 
+export class B extends Component {
+    type: string = "b";
+    decorater: (x: HTMLElement) => HTMLElement = (x) => {
+        x.style.fontWeight = "bold";
+        return x;
+    }
+}
+
 export class Button extends Component {
     type: string = "button";
 }
@@ -77,3 +95,120 @@ export class Ul extends Component {
 export class Li extends Component {
     type: string = "li";
 }
+
+
+export class H1 extends Component {
+    type: string = "h1";
+    constructor(text: string) {
+        super([], { innerText: text });
+    }
+}
+
+export class H2 extends Component {
+    type: string = "h2";
+}
+
+export class H3 extends Component {
+    type: string = "h3";
+}
+
+export class H4 extends Component {
+    type: string = "h4";
+}
+
+export class Form extends Component {
+    type: string = "form";
+    action: string;
+    method: 'get' | 'post';
+    constructor(
+        action: string,
+        method: 'get' | 'post',
+        children?: Component[], props?: Prop
+    ) {
+        super(children, props);
+        this.action = action;
+        this.method = method;
+    }
+
+    decorate(x: HTMLElement) {
+        x.setAttribute("action", this.action);
+        x.setAttribute("method", this.method);
+        return x;
+    }
+}
+
+
+export class Label extends Component {
+    type: string = "label";
+    forId: string;
+    formId: string;
+    constructor(forId: string, formId: string, children?: Component[], props?: Prop) {
+        super(children, props);
+        this.forId = forId;
+        this.formId = formId;
+    }
+
+    decorate(x: HTMLElement) {
+        x.setAttribute("for", this.forId);
+        x.setAttribute("form", this.formId);
+        return x;
+    }
+}
+
+export class Input extends Component {
+    type: string = "input";
+    name: string;
+    value: string;
+    inputType: 'text' | 'password' | 'email' | 'number' | 'date' | 'time' | 'checkbox' | 'radio' | 'submit' | 'reset' | 'button';
+    placeholder?: string;
+    required?: boolean;
+    constructor(
+        name: string, 
+        value: string, 
+        inputType: 'text' | 'password' | 'email' | 'number' | 'date' | 'time' | 'checkbox' | 'radio' | 'submit' | 'reset' | 'button',
+        placeholder?: string,
+        required?: boolean,
+        children?: Component[], props?: Prop
+    ) {
+        super(children, props);
+        this.name = name;
+        this.value = value;
+        this.inputType = inputType;
+        this.placeholder = placeholder;
+        this.required = required;
+    }
+
+    decorate(x: HTMLElement) {
+        x.setAttribute("name", this.name);
+        x.setAttribute("value", this.value);
+        x.setAttribute("type", this.inputType);
+        if (this.placeholder) x.setAttribute("placeholder", this.placeholder);
+        if (this.required) x.setAttribute("required", "true");
+        return x;
+    }
+}
+
+
+export class Table extends Component {
+    type: string = "table";
+}
+
+export class Td extends Component {
+    type: string = "td";
+}
+
+export class Th extends Component {
+    type: string = "th";
+}
+
+export class Tr extends Component {
+    type: string = "tr";
+}
+
+export class Tbody extends Component {
+    type: string = "tbody";
+}
+
+export class Thead extends Component {
+    type: string = "thead";
+}   
--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,9 @@
-import { Div, P, A, Button, Component, Ul, Li } from "../framework/node.ts";
+import { Div, P, A, Button, Component, Ul, Li, Input, H1 } from "../framework/node.ts";
 import { setTitle } from "../framework/helpers.ts";
 
 export default function App(): Component {
-  setTitle("home");
+  setTitle("todos");
   return new Div([
-    new P([], { innterText: "hello", className: "text-xl text-red-500" }),
-    new P([], { innterText: "world", className: "text-xl text-red-500" }),
-    new Button([], {
-      innterText: "click me",
-      onClick: () => {
-        alert("test");
-      },
-    }),
-    new A([], {
-      innterText: "google",
-      href: "https://www.google.com/",
-      target: "_",
-    }),
-
-    new Ul([
-      new Li([], { innterText: "get groseries" }),
-      new Li([], { innterText: "clean room" }),
-      new Li([], { innterText: "do homework" }),
-    ]),
-
-    Component.of("ol", [Component.of("Li", [], { innterText: "google" })]),
+    new H1("todos"),
   ]);
 }
